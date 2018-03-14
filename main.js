fa9d0417a21debee56a80a4fd5a310fe883bb839
jordandb <jordan.chenevier@gmail.com>
2018-03-14 20:12:07 +0100

    Committing the code

---
+++ main.js
@@ -0,0 +1,7 @@
+var exp = document.getElementById("exp");
+
+exp.addEventListener("click", function() {
+	document.getElementById("linkedin").style.display = "block";
+	document.getElementById("github").style.display = "block";
+	document.getElementById("angellist").style.display = "block";
+});
